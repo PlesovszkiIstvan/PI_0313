19eea5adee04c485b8860660e1b907c4d9ee6390
Plesovszki István <kilot58@gmail.com>
2023-03-23 09:01:45 +0100

    Bekötések és Cool layout hibajavítás

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const betet = document.querySelector('#betet');
+const tokesites_szam = document.querySelector('#tokesites_szam');
+const nevleges = document.querySelector('#nevleges');
+const Actual = document.querySelector('#Actual');
+const calcButton = document.querySelector('#calcButton');
+
+calcButton.addEventListener('click', ()=>{
+    console.log("test :)");
+    const betetszam = betet.value;
+    const tokesitesszam = tokesites_szam.value;
+    const nevlegesszam = nevleges.value;
+
+});
